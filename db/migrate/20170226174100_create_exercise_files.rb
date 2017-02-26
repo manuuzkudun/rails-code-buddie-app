f1e6e91cc78b62c421d77c359b963ad9d7f4caf6
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-26 17:41:26 +0000

    ExerciseFile created

---
+++ db/migrate/20170226174100_create_exercise_files.rb
@@ -0,0 +1,11 @@
+class CreateExerciseFiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :exercise_files do |t|
+      t.text :content
+      t.references :exercise, foreign_key: true
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
