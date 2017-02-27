5cb034af90a784a5e9b9c98466c103afd327a11f
natalia panferova <natalia.panferova.pro@gmail.com>
2017-02-27 13:19:04 +0000

    add text editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require codemirror
+//= require codemirror/modes/ruby
+//= require codemirror/modes/javascript
+//= require codemirror/modes/python
+//= require codemirror/modes/php
 //= require_tree .
