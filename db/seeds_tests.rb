d06c8cb4df54f86d02def76e5298aafaa474922c
Julia Mays <jm27746@gmail.com>
2017-02-28 08:28:28 +0000

    modified seed files to dynamically populate database from exercism website, added text area to display contents of test files to exercise show action

--- db/seeds_tests.rb
+++ db/seeds_tests.rb
@@ -1,39 +1,16 @@
-# DONE Create a folder with the different test files
-# create an array with paths of the exercises ['/exercises/helloworld', 'hff']
-#   or get access to the directory
+require 'open-uri'
+require 'nokogiri'
 
-# tests = ['/exercises/hello_world_test.rb', ...]
+exercises = Exercise.all
 
-
-# loop over the array of paths and then open the file how to read a file in ruby and grab the contents
-
-
-# :content will be what im reading from the file
-
-
-
-# open the files save the contents of the file in a variable content then create actual objects in the content
-
-
-# we need test file object
-
-
-# create exercise file objects for test files
-
-
-
-tests = ['db/exercises/hello_world_test.rb', 'db/exercises/hamming_test.rb', 'db/exercises/gigasecond_test.rb' ]
-tests.each do |test|
-  exercise_title = test.split('/')[2].split('.').slice(0).split('_').slice(0...-1).map {|w| w.capitalize}.join(' ')
-  exercise = Exercise.find_by(title: exercise_title)
-  content = ""
-  File.open(test, "r") do |f|
-    f.each_line do |line|
-      content += line
-    end
+# iterates through exercises
+exercises.each do |exercise|
+  url = "http://www.exercism.io/exercises/#{exercise.language.name.downcase}/#{exercise.title.parameterize}/test-suite"
+  html_file = open(url)
+  html_doc = Nokogiri::HTML(html_file)
+ # uses interpolation to define class name from language
+  test_file_selector = "div.highlight.#{exercise.language.name.parameterize} td.code"
+  html_doc.search(test_file_selector).each do |test_file|
+    exercise.exercise_files.create(content: test_file.text.remove("skip"), file_type: "Test File")
   end
-  ExerciseFile.create(content: content, file_type: 'test', exercise: exercise)
 end
-
-
-
