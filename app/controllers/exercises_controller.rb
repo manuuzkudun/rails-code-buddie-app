39781c4480c745606a1e24e2e81d3488ee5e18f6
natalia panferova <natalia.panferova.pro@gmail.com>
2017-02-28 10:42:29 +0000

    get the test output

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,6 +4,6 @@ class ExercisesController < ApplicationController
     # @value = "class SimpleNumber\ndef initialize(num)\nraise unless num.is_a?(Numeric)\n@x = num\nend\ndef add(y)\n@x + y\nend\ndef multiply(y)\n@x * y\nend\nend"
     @test = "require_relative \"code\"\nrequire \"test/unit\"\nclass TestSimpleNumber < Test::Unit::TestCase\ndef test_simple\nassert_equal(4, SimpleNumber.new(2).add(2) )\nassert_equal(4, SimpleNumber.new(2).multiply(2) )\nend\ndef test_typecheck\nassert_raise( RuntimeError ) { SimpleNumber.new(\'a\') }\nend\nend"
     @lang_id = "1"
-    @value = "#enter code here"
+    @value = "class SimpleNumber\ndef initialize(num)\nraise unless num.is_a?(Numeric)\n@x = num\nend\ndef add(y)\n@x + y\nend\ndef multiply(y)\n@x * y\nend\nend"
   end
 end
