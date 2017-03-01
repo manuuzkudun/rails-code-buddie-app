e6245decb81257528b86ec2a3574b49d2fca3b04
Julia Mays <jm27746@gmail.com>
2017-03-01 14:05:11 +0000

    implemented the exercise comment model and migrated

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,5 @@ class Exercise < ApplicationRecord
   belongs_to :language
   has_many :exercise_files
   has_many :exercise_submissions
+  has_many :exercise_comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228114255) do
+ActiveRecord::Schema.define(version: 20170301135923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "exercise_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["exercise_id"], name: "index_discussions_on_exercise_id", using: :btree
+  end
+
+  create_table "exercise_comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "exercise_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["exercise_id"], name: "index_exercise_comments_on_exercise_id", using: :btree
+    t.index ["user_id"], name: "index_exercise_comments_on_user_id", using: :btree
+  end
 
   create_table "exercise_files", force: :cascade do |t|
     t.text     "content"
@@ -69,6 +87,9 @@ ActiveRecord::Schema.define(version: 20170228114255) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "discussions", "exercises"
+  add_foreign_key "exercise_comments", "exercises"
+  add_foreign_key "exercise_comments", "users"
   add_foreign_key "exercise_files", "exercises"
   add_foreign_key "exercise_submissions", "exercises"
   add_foreign_key "exercise_submissions", "users"
