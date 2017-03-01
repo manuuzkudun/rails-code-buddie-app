ec78a966c45d3e4244b37f5ef5d451c6468cd8ca
Frederico Sanches <fred.sanches@gmail.com>
2017-03-01 15:12:50 +0000

    displayed the submitted exercises from every user for each exercise page

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,5 +9,18 @@ add_breadcrumb "Languages", :languages_path
     @value = "class SimpleNumber\ndef initialize(num)\nraise unless num.is_a?(Numeric)\n@x = num\nend\ndef add(y)\n@x + y\nend\ndef multiply(y)\n@x * y\nend\nend"
   add_breadcrumb @exercise.language.name, language_path(@exercise.language.id)
   add_breadcrumb @exercise.title, exercise_path(@exercise.id)
+    get_exercise_submissions
+
   end
+
+
+
+
+
+private
+
+def get_exercise_submissions
+@submissions = ExerciseSubmission.where(exercise_id: @exercise)
+
+end
 end
