fbdc077b71b22cf12d91e0ed71c308f2f81c128b
natalia panferova <natalia.panferova.pro@gmail.com>
2017-03-08 15:24:34 +0000

    submit only write solutions

--- app/controllers/exercise_submissions_controller.rb
+++ app/controllers/exercise_submissions_controller.rb
@@ -1,8 +1,7 @@
 class ExerciseSubmissionsController < ApplicationController
   before_action :authenticate_user!
   def create
-    puts "we are in create"
-    p ExerciseSubmission.create(
+    ExerciseSubmission.create(
       answer: params[:code],
       exercise: Exercise.find(params[:exercise]),
       user: current_user,
@@ -11,13 +10,12 @@ class ExerciseSubmissionsController < ApplicationController
   end
 
   def update
-    puts "we are in update"
     submission = ExerciseSubmission.where(
       user: current_user,
       exercise: Exercise.find(params[:exercise])
     )[0];
     submission.answer = params[:code]
-    p submission.save
+    submission.save
   end
 
 end
