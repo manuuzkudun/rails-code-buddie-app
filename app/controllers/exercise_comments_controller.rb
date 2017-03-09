cd8cbdffd9515e807b345c9ec018c6e374d875b1
natalia panferova <natalia.panferova.pro@gmail.com>
2017-03-09 11:54:08 +0000

    add comment form

--- app/controllers/exercise_comments_controller.rb
+++ app/controllers/exercise_comments_controller.rb
@@ -1,10 +1,19 @@
 class ExerciseCommentsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
     @exercise_comments = ExerciseComment.all
   end
 
   def create
+    p params
 
+    ExerciseComment.create(
+      content: params[:content],
+      user: current_user,
+      exercise: Exercise.find(params[:exercise])
+    )
   end
 
   def show
