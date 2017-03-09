1c2184f47a166f43bb6e1f008090d2a9f9e8f502
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-09 16:23:06 +0000

    video updated

--- app/assets/javascripts/video-conference.js
+++ app/assets/javascripts/video-conference.js
@@ -41,7 +41,7 @@ function initializeSession() {
   session.connect(token, function(error) {
     // If the connection is successful, initialize a publisher and publish to the session
     if (!error) {
-      var publisher = OT.initPublisher('publ', {
+      var publisher = OT.initPublisher('publisher', {
         insertMode: 'append',
         width: '100%',
         height: '100%'
