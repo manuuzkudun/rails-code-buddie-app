464fe6063c639e8499a9c15af7086f6af200a089
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-03-10 13:36:24 +0000

    collaborate redirection fixed

--- app/controllers/work_space_users_controller.rb
+++ app/controllers/work_space_users_controller.rb
@@ -4,19 +4,19 @@ class WorkSpaceUsersController < ApplicationController
   end
 
   def create
-    workspace = WorkSpace.find(params[:workspace])
+    workspace = ExerciseWorkSpace.where(name: params[:workspace])[0]
     if workspace.respond_to?(:exercise)
       workspace_user = WorkSpaceUser.create(
-        work_space: ExerciseWorkSpace.find(params[:workspace]),
+        work_space: ExerciseWorkSpace.where(name: params[:workspace])[0],
         user: User.find_by(email: params[:email]),
       )
-      redirect_to exercise_work_space_path(params[:workspace])
+      redirect_to exercise_work_space_path(workspace.id)
     else
       workspace_user = WorkSpaceUser.create(
-        work_space: PlaygroundWorkSpace.find(params[:workspace]),
+        work_space: PlaygroundWorkSpace.where(name: params[:workspace])[0],
         user: User.find_by(email: params[:email]),
       )
-      redirect_to playground_work_space_path(params[:workspace])
+      redirect_to playground_work_space_path(workspace.id)
     end
   end
 end
